d2684891b43d3a64d02290255ce4787a3dd760f4
Matthias Klatte <p.roni@proni.net>
2014-09-28 00:40:36 +0200

    Time functions created

--- lib/include.php
+++ lib/include.php
@@ -4,12 +4,16 @@ if (count($params) > 0) {
 	$lib = $params[0];
 	if (array_key_exists(1, $params) && $params[1]=="min" && file_exists('min/' . $lib . '.min.txt')) {
 		echo '<script><!--/* ' . $lib.' */' . "\n";
-		echo file_get_contents($lib . '.txt');
+		echo file_get_contents('min/' . $lib . '.min.txt');
 		echo '--></script>';
 	}
-	if (file_exists($lib . '.txt')) {
+	else if (file_exists($lib . '.txt')) {
 		echo '<script><!--/* ' . $lib.' */' . "\n";
 		echo file_get_contents($lib . '.txt');
+		echo '--></script>';
+	} else if (file_exists($lib . '.Script.txt')) {
+		echo '<script><!--/* ' . $lib.' */' . "\n";
+		echo file_get_contents($lib . '.Script.txt');
 		echo '--></script>';
 	} else {
 		echo '<script><!-- /* ' . $lib.'.txt not found! */ --></script>';
